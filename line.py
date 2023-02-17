baf69e69463d477f36a73a31e4488981c85f8f65
YuliyaStebakova <yuliadanya11@gmail.com>
2023-02-17 10:08:06 +0300

    check

---
+++ line.py
@@ -0,0 +1,6 @@
+mini = int(input("Введите минимальное число"))
+maxi = int(input("Введите максимальное число"))
+step = int(input("Введите шаговое число"))
+
+for i in range(mini, maxi, step):
+    print(i)
